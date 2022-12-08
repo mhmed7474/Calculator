418b5acb1991b6c2f43c786352afdf0765a013b4
Mohamed Emam <mhmedemam55@gmail.com>
2022-12-09 01:19:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var outPut = document.querySelector("input");
+var btns = document.querySelectorAll(".numbers");
+var clearBtn = document.querySelector(".clear");
+var equalBtn = document.querySelector(".equal");
+for (var x = 0; x < btns.length; x++) {
+    btns[x].addEventListener("click", (m) => {
+        outPut.value += m.target.dataset.number; 
+    });
+}
+equalBtn.addEventListener("click", () => {
+    if(outPut.value === "") {
+        outPut.value = "";
+    }else{
+        outPut.value = eval(outPut.value);
+    }
+});
+clearBtn.addEventListener("click", () => {
+    outPut.value = "";
+});
