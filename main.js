0d3412b7208c8a719ac467602e3093b74ccb3474
mhmed7474 <mhmedemam55@gmail.com>
2023-04-26 00:03:26 +0200

    first commit

--- main.js
+++ main.js
@@ -2,18 +2,20 @@ var outPut = document.querySelector("input");
 var btns = document.querySelectorAll(".numbers");
 var clearBtn = document.querySelector(".clear");
 var equalBtn = document.querySelector(".equal");
+var delBtn = document.querySelector(".delete");
 for (var x = 0; x < btns.length; x++) {
     btns[x].addEventListener("click", (m) => {
         outPut.value += m.target.dataset.number; 
     });
 }
 equalBtn.addEventListener("click", () => {
-    if(outPut.value === "") {
-        outPut.value = "";
-    }else{
-        outPut.value = eval(outPut.value);
-    }
+    outPut.value === "" 
+    ? outPut.value = ""
+    : outPut.value = eval(outPut.value);
 });
 clearBtn.addEventListener("click", () => {
     outPut.value = "";
 });
+delBtn.addEventListener("click" , () => {
+    outPut.value = outPut.value.slice(0 , -1);
+})
